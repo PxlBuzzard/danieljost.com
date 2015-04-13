b84333b457141fa13c9069497b135c0cd14fd12d
Daniel Jost <torpedojost@gmail.com>
2015-04-12 23:24:38 -0700

    Updated Brocfile.js so that things compile, even if it isn't complete.

--- Brocfile.js
+++ Brocfile.js
@@ -5,24 +5,20 @@ var findBowerTrees = require( 'broccoli-bower' );
 var compileNunjucks = require( 'broccoli-nunjucks' );
 
 // create tree for files in the app folder
-var src = 'src';
-src = pickFiles( src, {
-  srcDir: 'src',
-  destDir: 'out'
-});
+var pages = 'src/pages';
 
 //compile swig
-src = compileNunjucks( src, { 'include': 'src/partials' } );
+pages = compileNunjucks( pages, { 'include': 'src/partials' } );
 
 // create tree for files in the styles folder
-var styles = 'styles';
-styles = pickFiles( styles, {
+var styles = 'src/assets/scss';
+/*styles = pickFiles( styles, {
   srcDir: 'src/documents/scss',
   destDir: 'out/css'
-});
+});*/
 
 // include src and style trees
-var sourceTrees = [ src, styles ];
+var sourceTrees = [ pages, styles ];
 
 // Add bower dependencies
 sourceTrees = sourceTrees.concat( findBowerTrees() );
@@ -31,10 +27,10 @@ sourceTrees = sourceTrees.concat( findBowerTrees() );
 //var srcAndDependencies = new mergeTrees( sourceTrees, { overwrite: true } );
 
 // compile sass
-var appCss = compileSass( sourceTrees, 'out/css/site.scss', 'out/css/site.css' );
+//var appCss = compileSass( sourceTrees, 'src/assets/scss/site.scss', 'css/site.css' );
 
 // copy static files with no modifications
 var publicFiles = 'src/public';
 
 // merge js, css and public file trees, and export them
-module.exports = mergeTrees( [ appCss, publicFiles ] );
+module.exports = publicFiles;//mergeTrees( [ sourceTrees, publicFiles ] );
